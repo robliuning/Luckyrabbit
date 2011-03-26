7863ff6aac96fcc8e9815a9790d48f2c99df6dc1
luckyrabbit_developer1 <luckyrabbit_dev1@gmail.com>
2011-03-26 16:22:42 +0800

    update controller_employee

--- application/modules/general/models/DbTable/dept.php
+++ application/modules/general/models/DbTable/dept.php
@@ -1,6 +1,6 @@
 <?php
 
-class Application_Model_DbTable_Employee extends Zend_Db_Table_Abstract
+class Application_Model_DbTable_Dept extends Zend_Db_Table_Abstract
 {
     protected $_name = 'em_employees';
 }
