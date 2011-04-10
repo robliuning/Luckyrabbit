e755c6b87c324f186c26bd449c943ddc9cfb53c3
luckyrabbit_developer1 <luckyrabbit_dev1@gmail.com>
2011-04-10 14:05:54 +0800

    update subcontract

---
+++ application/modules/contract/controllers/SubcontractController.php
@@ -0,0 +1,146 @@
+<?php
+/*
+author:mingtingling
+date:2011.4.10
+vision:2.0
+*/
+class Contract_SubcontractController  extends Zend_Controller_Action
+{
+  public function init()
+	{
+     /*初始化*/
+	}
+  public function preDisPatch()
+	{
+     $this->view->render("_sidebar.phtml");
+	}
+  public function indexAction() /*addAction*/
+    {
+		$addForm=new Contract_Forms_SubcontractSave();
+		$addForm->submit->setLabel("保存新建");
+		$addForm->submit2->setAttrib('class','hide');
+		$subcontracts=new Contract_Models_SubcontractMapper();
+		if($this->getRequest()->isPost())
+		   {
+			$formData=$this->getRequest()->getPost();
+			if($addForm->isValid($formData))
+			    {
+                $subcontract=new Contract_Models_Subcontract();
+			    $subcontract->setProjectId($addForm->getValue('projectId'));
+				$subcontract->setScontrType($addForm->getValue('scontrType'));
+				$subcontract->setContractorId($addForm->getValue('contractorId'));
+				$subcontract->setScontrDetail($addForm->getValue('scontrDetail'));
+				$subcontract->setQuality($addForm->getValue('quality'));
+				$subcontract->setStartDateExp($addForm->getValue('startDateExp'));
+				$subcontract->setEndDateExp($addForm->getValue('endDateExp'));
+				$subcontract->setPeriodExp($addForm->getValue('periodExp'));
+				$subcontract->setStartDateAct($addForm->getValue('startDateAct'));
+				$subcontract->setEndDateAct($addForm->getValue('endDateAct'));
+				$subcontract->setPeriodAct($addForm->getValue('periodAct'));
+				$subcontract->setBrConContr($addForm->getValue('brConContr'));
+				$subcontract->setBrResContr($addForm->getValue('brResContr'));
+				$subcontract->setBrConSContr($addForm->getValue('brConSContr'));
+				$subcontract->setBrResSContr($addForm->getValue('brResSContr'));
+				$subcontract->setWarranty($addForm->getValue('warranty'));
+				$subcontract->setContrAmt($addForm->getValue('contrAmt'));
+				$subcontract->setConsMargin($addForm->getValue('consMargin'));
+                $subcontract->setPrjMargin($addForm->getValue('prjMargin'));
+				$subcontract->setPrjWarr($addForm->getValue('prjWarr'));
+				$subcontract->setRemark($addForm->getValue('remark'));
+                $subcontracts->save($subcontract);
+				$this->_redirect('/contract');
+			             }
+				       else
+			            {
+						   $addForm->populate($formData);
+				        }
+		   }
+		  $this->view->addForm=$addForm;
+	}
+public function editForm()
+	{
+	  $editForm=Contract_Forms_SubcontractSave();
+	  $editForm->submit->setLabel("保存修改");
+	  $editForm->submit2->setAttrib('class','hide');
+	  $subcontracts=new Contract_Models_SubcontractMapper();
+	  $scontrId=$this->_getParam('id',0);
+	  if($editForm->getRequest()->isPost())
+		   {
+		      $formData=$this->getRequest()->getPost();
+			  if($editForm->isValid($formData))
+			      {
+                $subcontract=new Contract_Models_Subcontract();
+				$subcontract->setScontrId($editForm->getValue('scontrId'));
+			    $subcontract->setProjectId($editForm->getValue('projectId'));
+				$subcontract->setScontrType($editForm->getValue('scontrType'));
+				$subcontract->setContractorId($editForm->getValue('contractorId'));
+				$subcontract->setScontrDetail($editForm->getValue('scontrDetail'));
+				$subcontract->setQuality($editForm->getValue('quality'));
+				$subcontract->setStartDateExp($editForm->getValue('startDateExp'));
+				$subcontract->setEndDateExp($editForm->getValue('endDateExp'));
+				$subcontract->setPeriodExp($editForm->getValue('periodExp'));
+				$subcontract->setStartDateAct($editForm->getValue('startDateAct'));
+				$subcontract->setEndDateAct($editForm->getValue('endDateAct'));
+				$subcontract->setPeriodAct($editForm->getValue('periodAct'));
+				$subcontract->setBrConContr($editForm->getValue('brConContr'));
+				$subcontract->setBrResContr($editForm->getValue('brResContr'));
+				$subcontract->setBrConSContr($editForm->getValue('brConSContr'));
+				$subcontract->setBrResSContr($editForm->getValue('brResSContr'));
+				$subcontract->setWarranty($editForm->getValue('warranty'));
+				$subcontract->setContrAmt($editForm->getValue('contrAmt'));
+				$subcontract->setConsMargin($editForm->getValue('consMargin'));
+                $subcontract->setPrjMargin($editForm->getValue('prjMargin'));
+				$subcontract->setPrjWarr($editForm->getValue('prjWarr'));
+				$subcontract->setRemark($editForm->getValue('remark'));
+				$subcontracts->save($subcontract);
+				$this->_redirect('/contract');
+			         } 
+				    else
+			         {
+						$editForm->populate($formData);
+					 }
+		   }
+		   else
+		   {
+			   if($scontrId>0)
+			      {
+				   $arraySubcontract=$subcontracts->findArraySubcontract($scontrId);
+				   $editForm->populate($arraySubcontract);
+			         }
+					 else
+			        {
+						 $this->_redirect('/contract');
+			        }
+		   }
+	}
+	public function ajaxdeleteAction()
+	{
+		$this->_helper->layout()->disableLayout();
+		$this->_helper->viewRenderer->setNoRender(true);
+		$scontrId=$this->_getParam('id',0);
+		if($scontrId>0)
+		   {
+			 $subcontracts=new Contract_Models_SubcontractMapper();
+			 $subcontracts->delete($scontrId);
+		      }
+              else
+		      {
+				  $this->_redirect('/contract');
+		      }
+	}
+	public function ajaxdisplayAction()
+	{
+		$scontrId=$this->_getParam('id',0);
+		if($scontrId>0)
+		   {
+			  $subcontracts=new Contract_Models_SubcontractMapper();
+			  $arraySubcontracts=$subcontracts->fetchAllSubcontracts();
+			  $this->view->arraySubcontracts=$arraySubcontracts;
+		       }
+			   else
+		      {
+				   $this->_redirect('/contract');
+		      }
+		   
+	}
+}
---
+++ application/modules/contract/forms/subcontractSave.php
@@ -0,0 +1,216 @@
+<?php
+/*
+author:mingtingling
+date:2011.4.10
+vision:2.0
+*/
+class Contract_Forms_SubcontractSave  extends Zend_Form
+{
+	public function init()
+	{
+		$this->setMethod('post');
+		$this->addElement(
+		 /*工程编号*/
+		'text','projectId',array(
+			 'label'=>'工程编号:',
+			 'required'=>true,
+			 'class'=>'tbLarge tbText'
+		   )
+		);
+		$this->addElement(
+		  /*分包类型*/
+		 'select','scontrType',array(
+			  'label'=>'分包类型:',
+			  'required'=>true,
+			  'multiOptions'=>array('1'=>'专业承包','2'=>'劳务分包'),
+			  'class'=>'tbLarge tbText'
+		   )
+		);
+	   $this->addElement(
+		  /*分包商编号*/
+	    'text','contractorId',array(
+		      'label'=>'分包商编号:',
+			  'required'=>true,
+			  'class'=>'tbLarge tbText'
+		  )
+	   );
+	  $this->addElement(
+	    /*分包项目及描述*/
+	    'text','scontrDetail',array(
+		      'label'=>'分包项目及描述:',
+			  'required'=>false,
+			  'class'=>'tbLarge tbText'
+		  )
+	  );
+	 $this->addElement(
+	   /*质量等级*/
+	  'select','quality',array(
+		    'label'=>'质量等级:',
+		    'required'=>false,
+		    'multiOptions'=>array('2'=>'优良','1'=>'合格','0'=>'基本合格'),
+		    'class'=>'tbLarge tbText'
+	   )
+	 );
+	 $this->addElement(
+	   /*预计开始日期*/
+	  'text','startDateExp',array(
+		    'label'=>'预计开始日期:',
+		    'required'=>false,
+		    'class'=>'tbLarge tbText'
+	    )
+	 );
+	$this->addElement(
+	   /*预计结束时间*/
+	   'text','endDateExp',array(
+		     'label'=>'预计结束时间:',
+		     'required'=>false,
+		     'class'=>'tbLarge tbText'
+	     )
+	);
+	$this->addElement(
+	   /*预计工期*/
+	    'text','periodExp',array(
+		      'label'=>'预计工期:',
+			  'required'=>false,
+			  'class'=>'tbText tbLarge'
+		 )
+	);
+	$this->addElement(
+	   /*实际开始时间*/
+	     'text','startDateAct',array(
+		       'label'=>'实际开始时间:',
+			   'required'=>false,
+			   'class'=>'tbText tbLarge'
+	     )
+	);
+	$this->addElement(
+	   /*实际结束时间*/
+	      'text','endDateAct',array(
+		       'label'=>'实际结束时间:',
+			   'required'=>false,
+			   'class'=>'tbLarge tbText'
+		  )
+	);
+	$this->addElement(
+	   /*实际工期*/
+	     'text','periodAct',array(
+		     'label'=>'实际工期:',
+			 'required'=>false,
+			 'class'=>'tbLarge tbText'
+		  )
+	);
+	$this->addElement(
+	   /*承包人违约情况*/
+	   'text','brConContr',array(
+		     'label'=>'承包人违约情况:',
+		     'required'=>false,
+		     'class'=>'tbLarge tbText'
+	     )
+	);
+	$this->addElement(
+	   /*承包人违约责任*/
+	   'text','brResContr',array(
+		    'label'=>'承包人违约责任:',
+		    'required'=>false,
+		    'class'=>'tbLarge tbText'
+	    )
+	);
+  $this->addElement(
+    /*分包商违约情况*/
+     'text','brConSContr',array(
+		 'label'=>'分包商违约责任:',
+		 'required'=>false,
+		 'class'=>'tbLarge tbText'
+	   )
+  );
+ $this->addElement(
+    /*分包商违约责任*/
+    'text','brResSContr',array(
+	     'label'=>'分包商违约责任:',
+		 'required'=>false,
+		 'class'=>'tbLarge tbText'
+	  )
+ );
+$this->addElement(
+   /*保修信息*/
+   'text','warranty',array(
+	      'label'=>'保修信息:',
+	      'required'=>false,
+	      'class'=>'tbLarge tbText'
+     )
+);
+$this->addElement(
+   /*合同造价*/
+   'text','contrAmt',array(
+	      'label'=>'合同造价:',
+	      'required'=>false,
+	      'class'=>'tbLarge tbText'
+     )
+);
+$this->addElement(
+   /*施工保证金*/
+   'text','consMargin',array(
+	     'label'=>'施工保证金:',
+	     'required'=>false,
+	     'class'=>'tbLarge tbText'
+     )
+);
+$this->addElement(
+   /*工程保证金*/
+   'text','prjMargin',array(
+	      'label'=>'工程保证金:',
+	      'required'=>false,
+	      'class'=>'tbLarge tbText'
+      )
+);
+$this->addElement(
+   /*工程保修金*/	
+  'text','prjWarr',array(
+	      'label'=>'工程保修金:',
+	      'required'=>false,
+	      'class'=>'tbLarge tbText'
+     )
+);
+$this->addElement(
+   /*备注*/
+   'textarea','remark',array(
+	      'label'=>'备注',
+	      'required'=>false,
+	      'class'=>'tbLarge tbText'
+      )
+);
+  $this->addElement(
+       /*按钮一*/
+       'submit','submit',array(
+		   'ignore'=>true,
+		   'class'=>'btConfirm radius',
+           'name'=>'submit'
+	     )
+  );
+$this->addElement(
+   /*按钮二*/
+    'submit','submit2',array(
+		   'ignore'=>true,
+		   'class'=>'btConfirm radius',
+		   'name'=>'submit'
+	    )
+);
+$this->setElementDecorators(
+      array(
+	    'ViewHelper',
+	    'Errors',
+	    array(array('data'=>'HtmlTag'),
+	    array('tag'=>'td','class'=>'element')),
+	    array('Label',array('tag'=>'td')),
+	    array(array('row'=>'HtmlTag'),array('tag'=>'tr'))
+      )
+ );
+ $this->setDecorators(
+	    array(
+       	 'FormElements',
+	     array('HtmlTag',array('tag'=>'table')),
+	     'Form'
+     )
+  );
+	}
+}
