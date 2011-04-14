82dbac03dbe8b7e7893c871061e70b022b643834
luckyrabbit_developer1 <luckyrabbit_dev1@gmail.com>
2011-04-14 18:36:28 +0800

    purchasecontroller

--- application/modules/material/controllers/PurchaseController.php
+++ application/modules/material/controllers/PurchaseController.php
@@ -16,22 +16,138 @@ class Material_PurchaseController extends Zend_Controller_Action
 
     public function indexAction()
     {
-	
+	$purchases = new Material_Models_PurchaseMapper();
+		
+		if($this->getRequest()->isPost())
+		{
+			$formData = $this->getRequest()->getPost();
+			
+			$key = $formData['key'];
+			$condition = $formData['condition'];
+			$arrayPurchases = $purchases->fetchAllJoin($key,$condition);
+		}
+		else
+		{
+			$arrayPurchases = $purchases->fetchAllJoin();
+			}
+		
+		$this->view->arrayPurchases = $arrayPurchases;
     }
     
     public function addAction()
     {
-   
+       	$addForm = new Material_Forms_purchaseSave();
+		$addForm->submit->setLabel('保存继续新建');
+		$addForm->submit2->setLabel('保存返回上页');
+
+		$purchases = new Material_Models_PurchaseMapper();
+		$purchases->populatePurchaseDd($addForm);
+
+		if($this->getRequest()->isPost())
+		{
+			$btClicked = $this->getRequest()->getPost('submit');
+			$formData = $this->getRequest()->getPost();
+			if($addForm->isValid($formData))
+			{
+				$purchase = new Material_Models_Purchase();
+				$purchase->setProjectId($addForm->getValue('projectId'));
+				$purchase->setVenId($addForm->getValue('venId'));
+				$purchase->set	BuyerId($addForm->getValue('buyerId'));
+				$purchase->setDate($addForm->getValue('date'));
+				$purchase->setDestId($addForm->getValue('destId'));
+				$purchase->setFreight($addForm->getValue('freight'));
+				$purchase->setInvoice($addForm->getValue('invoice'));
+				$purchase->setRemark($addForm->getValue('remark'));
+				$purchases->save($purchase);
+				if($btClicked=='保存继续新建')
+				{
+					$addFrom->getElement('projectId')->setValue('');
+					$addFrom->getElement('venId')->setValue('');
+					$addFrom->getElement('buyerId')->setValue('');
+					$addFrom->getElement('date')->setValue('');
+					$addFrom->getElement('destId')->setValue('');
+					$addFrom->getElement('freight')->setValue('');
+					$addFrom->getElement('invoice')->setValue('');
+					$addFrom->getElement('remark')->setValue('');
+					}
+					else
+					{
+						$this->_redirect('/material/purchase');
+						}
+			}
+			else
+			{
+				$this->populate($formData);
+			}
+		}
+		 $this->view->addForm = $addForm;
    	}
     
     public function editAction()
     {
+$editForm = new Material_Forms_purchaseSave();
+		$editForm->submit->setLabel('保存修改');
+    	$editForm->submit2->setAttrib('class','hide');
 
+		$purchases = new Material_Models_PurchaseMapper();
+    	$purId = $this->_getParam('id',0);
+
+		if($this->getRequest()->isPost())
+		{
+			$formData = $this->getRequest()->getPost();
+    		if($editForm->isValid($formData))
+			{
+				$purchase = new Material_Models_Purchase();
+				$purchase->setProjectId($editForm->getValue('projectId'));
+				$purchase->setVenId($editForm->getValue('venId'));
+				$purchase->set	BuyerId($editForm->getValue('buyerId'));
+				$purchase->setDate($editForm->getValue('date'));
+				$purchase->setDestId($editForm->getValue('destId'));
+				$purchase->setFreight($editForm->getValue('freight'));
+				$purchase->setInvoice($editForm->getValue('invoice'));
+				$purchase->setRemark($editForm->getValue('remark'));
+				$purchases->save($purchase);
+
+				$this->_redirect('/material/purchase');
+			}
+			else
+    			{
+    				$editForm->populate($formData);
+    				}
+		}
+		else
+    	{
+    		if($purId >0)
+    		{
+    			$arraypurchase = $purchases->findArrayPurchase($purId);
+    			$editForm->populate($arraypurchase);
+    			}
+    			else
+    			{
+    				$this->_redirect('/material/purchase');
+    				}
+    		}		
+    	$this->view->editForm = $editForm;
+    	$this->view->id = $purId; 
     }
     
     public function ajaxdeleteAction()
     {
-
+    	$this->_helper->layout()->disableLayout();
+    	$this->_helper->viewRenderer->setNoRender(true);
+   
+   
+   		$purId = $this->_getParam('id',0);
+    	if($purId > 0)
+    	{
+    		$purchases = new Material_Models_PurchaseMapper();
+    		$purchases->delete($purId);
+    		echo "1";
+    		}
+    		else
+    		{
+    			$this->_redirect('/material/purchase');
+    			}
     }
 }
 
---
+++ application/modules/material/forms/purchaseSave.php
@@ -0,0 +1,104 @@
+<?php
+	/*
+	Created by Meimo
+	Date 2011.4.1
+	*/
+class Material_Forms_purchaseSave extends Zend_Form
+{
+    public function init()
+    {
+    	$this->setMethod('post');
+			
+		$this->addElement(
+			'select', 'projectId', array(
+			'label' => '工程名称: ',
+			'required' => true,
+			'class'=>'tbLarge tbText'
+			)
+		);
+     	$this->addElement(
+			'select', 'venId', array(
+			'label' => '供应商: ',
+			'required' => true,
+			'class'=>'tbLarge tbText'
+			)
+		);
+	  	$this->addElement(
+			'text', 'buyerId', array(
+			'label' => '采购员: ',
+			'required' => true,
+			'class'=>'tbMedium tbText'
+			)
+		);
+    	$this->addElement(
+			'text', 'Date', array(
+			'label' => '采购日期: ',
+			'required' => false,
+			'class'=>'tbLarge tbText'
+			)
+		);
+		$this->addElement(
+			'select', 'destId', array(
+			'label' => '入库地: ',
+			'required' => true,
+			'class'=>'tbMedium tbText'
+			)
+		);
+	   $this->addElement(
+			'text', 'freight', array(
+			'label' => '运费: ',
+			'required' => false,
+			'class'=>'tbLarge tbText'
+			)
+		);
+	   	$this->addElement(
+			'text', 'invoice', array(
+			'label' => '原始单号: ',
+			'required' => false,
+			'class'=>'tbLarge tbText'
+			)
+		);
+		$this->addElement(
+			'textarea', 'remark', array(
+			'label' => '备注: ',
+			'required' => false,			
+			'class'=>'tbLarge tbText',
+			'cols'=>40,
+			'rows'=>5
+			)
+		);
+
+    	$this->addElement(
+    		'submit','submit',array(
+    		'ignore'=>true,
+    		'class'=>'btConfirm radius',
+    		'name'=>'submit'
+    		)
+    	);
+    	
+    	$this->addElement(
+    		'submit','submit2',array(
+    		'ignore'=>true,
+    		'class'=>'btConfirm radius',
+    		'name'=>'submit'
+    		)
+    	);
+    	
+    	$this->setElementDecorators(array(
+            'ViewHelper',
+            'Errors',
+            array(array('data'=>'HtmlTag'),
+            array('tag'=>'td','class'=>'element')),
+            array('Label',array('tag'=>'td')),
+            array(array('row'=>'HtmlTag'),array('tag'=>'tr')),
+
+   		 ));
+
+		$this->setDecorators(array(
+            'FormElements',
+            array('HtmlTag',array('tag'=>'table')),
+            'Form'
+        ));
+    }
+}
+?>
