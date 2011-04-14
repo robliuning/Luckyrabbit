d7f5fe31f6346f68fb079bbd2e52934cb6a7be57
luckyrabbit_developer1 <luckyrabbit_dev1@gmail.com>
2011-04-14 14:13:15 +0800

    material controller

--- application/modules/material/controllers/IndexController.php
+++ application/modules/material/controllers/IndexController.php
@@ -1,5 +1,6 @@
 <?php
-
+//Author: Meimo
+//Date: 2011.4.14
 class Material_IndexController extends Zend_Controller_Action
 {
 
@@ -16,27 +17,138 @@ class Material_IndexController extends Zend_Controller_Action
     public function indexAction()
     {
         // action body
+		$materials = new Material_Models_MaterialMapper();
+		$this->view->arrayMaterial = $materials->fetchAllJoin();
     }
     
     public function addAction()
     {
-    
+    $addForm = new Material_Forms_MaterialSave();
+	$addForm->submit->setLable('保存并继续添加');
+	$addform->submit2->setLable('保存并返回');
+
+	if($this->getRequest()->isPost())
+		{
+		$btClicked = $this->getRequest()->getPost('submit');
+		$formData = $this->getRequest()->getPost();
+		if($addForm->isValid($formData))
+			{
+			$material = new Material_Models_Material();
+			$material->setName($addFrom->getValue('name'));
+			$material->setType($addFrom->getValue('type'));
+			$material->setSpec($addFrom->getValue('spec'));
+			$material->setUnit($addFrom->getValue('unit'));
+			$material->setRemark($addFrom->getValue('remark'));
+			$materials->save($material);
+			if($btClicked=='保存并继续添加')
+				{
+				$addFrom->getElement('name')->setValue('');
+				$addFrom->getElement('type')->setValue('');
+				$addFrom->getElement('spec')->setValue('');
+				$addFrom->getElement('unit')->setValue('');
+				$addFrom->getElement('remark')->setValue('');
+			}
+			else
+				{
+				$this->_redirect('/material');
+			}
+			else
+				{
+				$this->populate($formData);
+			}
+		}
+		 $this->view->addForm = $addForm;
+	}
+	
+
     }
     
     public function editAction()
     {
-    
+    $editForm = new Material_Models_MaterialSave();
+	$editForm->submit->setLabel('保存修改');
+    $editForm->submit2->setAttrib('class','hide');
+
+	$materials = new Material_Models_MaterialMapper();
+    $mtrId = $this->_getParam('id',0);
+
+	if($this->getRequest()->isPost())
+		{
+		$formData = $this->getRequest()->getPost();
+    		if($editForm->isValid($formData))
+			{
+				$material = new Material_Models_Material();
+				$material->setMtrId($mtrId);
+				$material->setName($editForm->getValue('name'));
+				$material->setType($editForm->getValue('type'));
+				$material->setSpec($editForm->getValue('spec'));
+				$material->setUnit($editForm->getValue('unit'));
+				$material->setRemark($editForm->getValue('remark'));
+				$materials->save($material);
+
+				$this->_redirect('/material');
+			}
+			else
+    			{
+    				$editForm->populate($formData);
+    				}
+	}
+	else
+    		{
+    			if($mtrId >0)
+    			{
+    			    $arrayMaterial = $materials->findArrayMaterial($mtrId);
+    				$editForm->populate($arrayMaterial);
+    				}
+    				else
+    				{
+    					$this->_redirect('/material');
+    					}
+    			}		
+    	$this->view->editForm = $editForm;
+    	$this->view->id = $material; 
+
     }
     
     public function ajaxdeleteAction()
     {
-    
+    $this->_helper->layout()->disableLayout();
+    	$this->_helper->viewRenderer->setNoRender(true);
+   
+   
+   		$mtrId = $this->_getParam('id',0);
+    	if($mtrId > 0)
+    	{
+    		$materials = new Material_Models_MaterialMapper();
+    		$materials->delete($mtrId);
+    		echo "1";
+    		}
+    		else
+    		{
+    			$this->_redirect('/material');
+    			}
     }
     
     public function searchAction()
     {
     	//key => user input 
     	//conditon => material name,type and spc
+		$materials = new Material_Models_MaterialMapper();
+
+		if($this->getRequest()->isPost())
+		{
+			$formData = $this->getRequest()->getPost();
+			$material = new Material_Models_Material();
+				$material->setMtrId($mtrId);
+				$material->setName($editForm->getValue('name'));
+				$material->setType($editForm->getValue('type'));
+				$material->setSpec($editForm->getValue('spec'));
+				$material->setUnit($editForm->getValue('unit'));
+				$material->setRemark($editForm->getValue('remark'));
+				$materials->fetchAllJoin()
+		}
+
+
     }
 }
 
---
+++ application/modules/material/forms/materialSave.php
@@ -0,0 +1,87 @@
+<?php
+	/*
+	Created by Meimo
+	Date 2011.4.14
+	review: rob
+	Date 2011.4.3
+	*/
+class Project_Forms_ProjectSave extends Zend_Form
+{
+    public function init()
+    {
+    	$this->setMethod('post');
+			
+		$this->addElement(
+			'text', 'name', array(
+			'label' => '材料名称: ',
+			'required' => true,
+			'class'=>'tbLarge tbText'
+			)
+		);
+     	$this->addElement(
+			'text', 'typeId', array(
+			'label' => '类型: ',
+			'required' => true,
+			'class'=>'tbLarge tbText'
+			)
+		);
+	  	$this->addElement(
+			'text', 'spec', array(
+			'label' => '规格型号: ',
+			'required' => false,
+			'class'=>'tbLarge tbText'
+			)
+		);
+    	$this->addElement(
+			'text', 'unit', array(
+			'label' => '单位: ',
+			'required' => true,
+			'class'=>'tbLarge tbText'
+			)
+		);
+
+
+		$this->addElement(
+			'textarea', 'remark', array(
+			'label' => '备注: ',
+			'required' => false,			
+			'class'=>'tbLarge tbText',
+			'cols'=>40,
+			'rows'=>5
+			)
+		);
+
+    	$this->addElement(
+    		'submit','submit',array(
+    		'ignore'=>true,
+    		'class'=>'btConfirm radius',
+    		'name'=>'submit'
+    		)
+    	);
+    	
+    	$this->addElement(
+    		'submit','submit2',array(
+    		'ignore'=>true,
+    		'class'=>'btConfirm radius',
+    		'name'=>'submit'
+    		)
+    	);
+    	
+    	$this->setElementDecorators(array(
+            'ViewHelper',
+            'Errors',
+            array(array('data'=>'HtmlTag'),
+            array('tag'=>'td','class'=>'element')),
+            array('Label',array('tag'=>'td')),
+            array(array('row'=>'HtmlTag'),array('tag'=>'tr')),
+
+   		 ));
+
+		$this->setDecorators(array(
+            'FormElements',
+            array('HtmlTag',array('tag'=>'table')),
+            'Form'
+        ));
+    }
+}
+?>
